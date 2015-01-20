c1ba238b13d1ee07231b636b7d22f217b63af798
friendzis <me@friendzis.net>
2015-01-20 14:08:50 +0200

    reworked 12. SHould be faster

--- 12.py
+++ 12.py
@@ -1,12 +1,31 @@
+import math
+
 def triangles():
-    t = 0
-    i = 0
+    t = 1
+    i = 1
     while True:
         t, i = t + i + 1, i + 1
         yield t
 
+table = {}
+
+def factors(num):
+    f = set([num])
+    if num == 1:
+        return set([1])
+    if num in table:
+        return table[num]
+    for i in reversed(range(1, math.ceil(num/2)+1)):
+        if num % i == 0:
+            f = f | factors(i)
+    table[num] = f
+    return f
+
+
 for t in triangles():
-    count = len([i for i in range(1, t+1) if not t % i])
-    if count > 500:
-        print(t)
+    l = len(factors(t))
+    if l > 500:
+        print("%d has %d" % (t, l))
         break
+
+
